a865765e91db47fcbf89735ab71c7147f14a252c
Mark Vayngrib <mark@tradle.io>
2019-08-13 12:20:49 -0400

    add breaking toJSON test

--- test/index.js
+++ test/index.js
@@ -6,4 +6,5 @@ describe('Test specs', function () {
   require('./ed25519-test.js');
   require('./use-precomputed-test.js');
   require('./validate-preset-curves-test.js');
+  require('./toJSON-test.js');
 });
